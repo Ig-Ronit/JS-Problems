5d4d6b9bf14dc60182650357fc0256a9354a7461
Ig-Ronit <ronitkene7@gmail.com>
2025-09-16 17:13:30 +0530

    Adding Solutions

--- kth_mostFrequentElement.js
+++ kth_mostFrequentElement.js
@@ -9,3 +9,19 @@
 // Input: array of integers nums, integer k
 // Output: the k most frequent elements.
 // Order: By frequency (not by numeric value).
+
+function topKFrequent(nums, k) {
+
+  const freqMap = new Map();
+  for (let num of nums) {
+    freqMap.set(num, (freqMap.get(num) || 0) + 1);
+  }
+
+  const sorted = [...freqMap.entries()].sort((a, b) => b[1] - a[1]);
+  return sorted.slice(0, k).map(entry => entry[0]);
+
+
+}
+
+console.log(topKFrequent([1, 1, 1, 1, 2, 2, 2, 2, 2, 23, 3, 3, 3, 3, 34, 4, 4, 4, 4, 5, 5,], 3)); // [2,1,3]
+console.log(topKFrequent([1, 1, 1, 2, 3, 3, 5, 5, 5, 5], 3)); // [5,1,3]
