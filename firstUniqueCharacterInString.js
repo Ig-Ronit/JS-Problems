5e043faaa83fcc952532680c006b08e9c8777a58
Ig-Ronit <ronitkene7@gmail.com>
2025-09-17 20:12:58 +0530

    Added Solution

--- firstUniqueCharacterInString.js
+++ firstUniqueCharacterInString.js
@@ -7,3 +7,18 @@
 // Example:
 // Input: "leetcode" → Output: 0
 // Input: "loveleetcode" → Output: 2
+
+function firstUniqChar(s) {
+  const charCount = {};
+  for (let char of s) {
+    charCount[char] = (charCount[char] || 0) + 1;
+
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (charCount[s[i]] === 1) {
+      return i;
+    }
+  }
+}
+console.log(firstUniqChar("loveleetcode")); // Output: 2
+console.log(firstUniqChar("leetcode")); // Output: 0
